ff55c6aef7cc0765cac86192101aab28f911bd83
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-18 10:22:16 +0100

    Add list of task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
 console.log('You have some work to do!');
+
+const {createApp} = Vue 
+
+createApp({
+    data(){
+     return {
+        tasks:[{
+            text:'Fare colazione',
+            done:false,
+        },
+        {
+            text:'Prepararsi',
+            check:true,
+        },
+        {
+            text:'Seguire le lezioni',
+            check:false,
+        },
+    ]
+     }
+    },
+    methode(){
+
+    }
+
+}).mount('#app')
